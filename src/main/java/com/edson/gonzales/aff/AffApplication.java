d16ef81e2244d506f671feb73731ff17e0fdbf4e
Reynaldo Edson Gonzales Ramos <edson7mayo@gmail.com>
2025-03-31 09:32:55 +0200

    Inicio de proyecto: Declaración de entidades y tablas a utilizar en la futura base de datos

---
+++ src/main/java/com/edson/gonzales/aff/AffApplication.java
@@ -0,0 +1,13 @@
+package com.edson.gonzales.aff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AffApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AffApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/edson/gonzales/aff/AffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edson.gonzales.aff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AffApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

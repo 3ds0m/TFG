5d0c867dacd2112dfe61e8ea84ac27090268f986
Reynaldo Edson Gonzales Ramos <edson7mayo@gmail.com>
2025-04-01 15:57:40 +0200

    Octavo Commit: Configurar CORS en el backend para permitir acceso solo al futuro frontend

---
+++ src/main/java/com/edson/gonzales/aff/Config/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.edson.gonzales.aff.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*") // Durante el desarrollo permite pero luego poner un https://frontend.com
+                        .allowedMethods("GET", "POST", "PUT", "DELETE") // Metodos que permite mi back
+                        .allowCredentials(true);
+            }
+        };
+    }
+}

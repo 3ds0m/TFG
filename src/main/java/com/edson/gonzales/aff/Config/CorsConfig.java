80930ff2607f4bdff3842a71d2d16f1bd1b9903f
Reynaldo Edson Gonzales Ramos <edson7mayo@gmail.com>
2025-05-27 14:15:11 +0200

    Usuarios

--- src/main/java/com/edson/gonzales/aff/Config/CorsConfig.java
+++ src/main/java/com/edson/gonzales/aff/Config/CorsConfig.java
@@ -2,6 +2,8 @@ package com.edson.gonzales.aff.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -10,12 +12,13 @@ import java.util.Arrays;
 
 @Configuration
 public class CorsConfig {
-
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(Arrays.asList("https://edsontfg.vercel.app")); // ✅ Solo Vercel
-        config.setAllowedMethods(Arrays.asList("GET", "POST")); // ✅ GET para obtener, POST para pago
+
+        config.setAllowedOrigins(Arrays.asList("http://127.0.0.1:5500","https://edsontfg.vercel.app")); // Vercel https://edsontfg.vercel.app
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        // GET para obtener, POST para pago
         config.setAllowCredentials(true);
         config.setAllowedHeaders(Arrays.asList("*")); // Permitir todos los headers necesarios
 
@@ -23,5 +26,9 @@ public class CorsConfig {
         source.registerCorsConfiguration("/**", config); // Aplica a toda la API
 
         return new CorsFilter(source);
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
